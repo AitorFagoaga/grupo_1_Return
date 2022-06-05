920d4a9d71ae36daca61075e21f1d9fe36f1de18
Bruno Secchiari <brunosecchiari95@gmail.com>
2022-06-04 22:14:26 -0300

    Trabajando en el Sprint 4

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const methodOverride =  require('method-override');
 const path = require('path');
 
 const app = express();
+
 const mainRoutes = require('./routes/main');
 const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
@@ -9,7 +11,10 @@ const usersRoutes = require('./routes/users');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/', mainRoutes);
 app.use('/products', productsRoutes);
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,7 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../database/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const mainController = {
 
     index: (req, res) => {
-        res.render('./index');
+        const productsOnSale = products.filter(product => {
+            return product.category == 'On Sale';
+        })
+
+        const productsTrending = products.filter(product => {
+            return product.category == 'Trending';
+        })
+
+        res.render('./index', {productsOnSale: productsOnSale, productsTrending: productsTrending});
     }
 
 }
