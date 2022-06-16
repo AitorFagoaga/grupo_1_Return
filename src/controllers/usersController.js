445c0c712176b1d396b5514bd0eeb96153c9ae97
AitorFagoaga <aitor.fagoaga2001@gmail.com>
2022-06-16 20:25:44 -0300

    register validations

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,13 +1,23 @@
+const { validationResult } = require('express-validator');
+
 const usersController = {
 
     login: (req, res) => {
-        res.render('./users/login');
+        return res.render('./users/login');
     },
 
     register: (req, res) => {
-        res.render('./users/register');
+        return res.render('./users/register');
+    },
+    processRegister: (req,res) => {
+       const resultValidation =  validationResult(req);
+       if(resultValidation.errors.length > 0){
+           return res.render('./users/register',{ errors: resultValidation.mapped(),
+            oldData: req.body
+        });
+       };
     }
 
-}
+};
 
 module.exports = usersController;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,20 @@
+
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const { body } = require('express-validator');
+
+const validations = [
+    body('name').notEmpty().withMessage('Debes poner tu nombre'),
+    body('email')
+        .notEmpty().withMessage('Debes colocar un email').bail()
+        .isEmail().withMessage('Debes colocar un formato de email valido'),
+    body('password').notEmpty().withMessage('La contraseña debe de tener menos de 8 caracteres')
+];
 
 router.get('/login', usersController.login);
+
 router.get('/register', usersController.register);
+router.post('/register', validations ,usersController.processRegister);
 
 module.exports = router;
