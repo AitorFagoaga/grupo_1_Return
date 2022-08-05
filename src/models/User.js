7d5073d9d64fd060732f3907bbb66061550c3e2e
Bruno-Morales <br1_morales@hotmail.com>
2022-08-05 18:16:49 -0300

    Generando la migracion a la BD, de los usuarios nuevamente

--- src/models/User.js
+++ src/models/User.js
@@ -1,64 +1,51 @@
-const fs = require('fs');
-const path = require('path');
-const archivoRuta= path.join(__dirname, '../data/users.json');
-const db = require("../database/models")
-
-
+const fs = require("fs");
+const path = require("path");
+const db = require("../database/models");
 
 const User = {
+  //encontrar a todos los usuarios
+  findAllUsers: function () {
+    db.Users.findAll().then(function (user) {
+      return user;
+    });
+  },
+  //buscar usuario por id ( pk = private key )
+  findByPk: function (id) {
+    db.Users.findByPk(id).then(function (user) {
+      return user;
+    });
+  },
 
-    //encontrar a todos los usuarios
-    findAllUsers: function () {
-        db.Users.findAll()
-        .then(function(user){
-            return user
-        })
-    },
-    //buscar usuario por id ( pk = private key )
-    findByPk: function (id)  {
+  findByField: function (field, value) {
+    const email = field;
 
-        db.Users.findByPk(id)
-        .then(function(user){
-            return user
-        })
+    db.Users.findOne({
+      where: email == value,
+    }).then((resultado) => {
+      return resultado;
+    });
+  },
 
-    }, 
+  create: function (userData) {
+    let newUser = db.Users.create({
+      name: userData.name,
+      category: userData.category,
+      email: userData.email,
+      password: userData.password,
+      image: userData.filename,
+    });
 
-    findByField: function (field,value) {
-
-        const email = field;
-
-        db.Users.findOne({
-            where: email == value
-        })
-        .then((resultado) => {
-            return resultado
-        })
-
-    }, 
-
-    create: function (userData) {
-
-        let newUser = db.Users.create({
-            name:userData.name,
-            category: userData.category,
-            email: userData.email,
-            password: userData.password,
-            image : userData.filename,
-        })
-        
-        return newUser;
-    },
-    //borrar usuario
-    delete: function (id){
-         db.Users.destroy({
-            where:{
-                id: id
-            }
-        })
-        res.redirect('/products/productList')
-    }
-}
-
+    return newUser;
+  },
+  //borrar usuario
+  delete: function (id) {
+    db.Users.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.redirect("/products/productList");
+  },
+};
 
 module.exports = User;
