25a519342c8d8562081ece8f99075deb6de77581
Bruno-Morales <br1_morales@hotmail.com>
2022-08-20 01:47:54 -0300

    Javascript del Front-Validaciones Falta CSS

--- public/js/productsValidation.js
+++ public/js/productsValidation.js
@@ -1,49 +1,35 @@
-window.addEventListener("load", function (){
-    
-    let form = document.querySelector('.form');
-    //form.titulo.focus();
-    form.addEventListener('submit', (e) => {
-        
-        let errors = [];
+window.onload = function () {
+  let form = document.querySelector("#form");
 
-        let name = document.querySelector('#name')
-        let description = document.querySelector('#description');
-        let price = document.querySelector('#price');
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-        if (name.value == '' ) {
-            errors.push('El nombre no puede estar vacio');
-            if(name.value.length > 3){
-                errors.push('El nombre tiene que ser mayor a 3 caracteres');
-            }
-        } else {
-            form.price.focus();
-        };
-        if (price.value < 0) {
-            errors.push('El precio no puede ser 0');
-        } else {
-            form.description.focus();
-        };
-        if (description == "") {
-            errors.push('El producto debe tener una descripcion');
-        }
-        
-        
-        
+    let errors = [];
 
-        if (errors.length > 0) {
-            e.preventDefault();
-            let losErrores = document.querySelector('.errors');
-            losErrores.innerHTML = '';
-            for (let i = 0; i < errors.length; i++) {
-                losErrores.innerHTML += `<li >  ${errors[i]} </li>`;
-            };
-        }else{
-            alert('Todo ok')
-            form.submit();
-        }
+    let name = document.querySelector("#name");
+    let price = document.querySelector("#price");
+    let description = document.querySelector("#descripcion");
 
-    });
+    if (name.value == "") {
+      errors.push("El nombre no puede estar vacio");
+    }
+    if (price.value <= 0) {
+      errors.push("El precio no puede ser 0");
+    }
+    if (description.value == "") {
+      errors.push("El producto debe tener una descripcion");
+    }
 
+    console.log(errors);
 
-})
-
+    if (errors.length > 0) {
+      let losErrores = document.querySelector("#errors");
+      losErrores.innerHTML = "";
+      for (let i = 0; i < errors.length; i++) {
+        losErrores.innerHTML += `<li>${errors[i]}</li>`;
+      }
+    } else {
+      form.submit();
+    }
+  });
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,55 +1,63 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const usersController = require('../controllers/usersController');
-const { body } = require('express-validator');
-const multer = require('multer');
-const loggedUserMiddleware = require('../middlewares/loggedUserMiddleware');
-const profileAuthMiddleware = require('../middlewares/profileAuthMiddleware');
-
+const path = require("path");
+const usersController = require("../controllers/usersController");
+const { body } = require("express-validator");
+const multer = require("multer");
+const loggedUserMiddleware = require("../middlewares/loggedUserMiddleware");
+const profileAuthMiddleware = require("../middlewares/profileAuthMiddleware");
 
 const storage = multer.diskStorage({
-    destination: (req,file,cb) => {
-        cb(null, path.join(__dirname, "../../public/images/users"));
-    },
-    filename: (req,file,cb) => {
-        let imageName = Date.now() + "-" +  path.extname(file.originalname);
-        cb(null, imageName);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../public/images/users"));
+  },
+  filename: (req, file, cb) => {
+    let imageName = Date.now() + "-" + path.extname(file.originalname);
+    cb(null, imageName);
+  },
+});
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 const validations = [
-    body('name').notEmpty().withMessage('Debes poner tu nombre'),
-    body('category').notEmpty().withMessage('Debes elegir una categoria'),
-    body('image').custom((value, {req})=>{
-        let image = req.file;
-        let fileExtention = path.extname(image.originalname);
-        let extensions = ['.jpg','.png'];
-        if(!image){
-            throw new Error ("Debes colocar una foto de perfil")
-        }else{
-            if(!extensions.includes(fileExtention)){
-                throw new Error ("Debes colocar una imagen con .jpg o .png")
-            }
-        }
-        return true;
-    }),
-    body('email')
-        .notEmpty().withMessage('Debes colocar un email').bail()
-        .isEmail().withMessage('Debes colocar un formato de email valido'),
-    body('password').notEmpty().withMessage('La contraseña debe de tener menos de 8 caracteres')
+  body("name").notEmpty().withMessage("Debes poner tu nombre"),
+  body("category").notEmpty().withMessage("Debes elegir una categoria"),
+  body("image").custom((value, { req }) => {
+    let image = req.file;
+    let fileExtention = path.extname(image.originalname);
+    let extensions = [".jpg", ".png"];
+    if (!image) {
+      throw new Error("Debes colocar una foto de perfil");
+    } else {
+      if (!extensions.includes(fileExtention)) {
+        throw new Error("Debes colocar una imagen con .jpg o .png");
+      }
+    }
+    return true;
+  }),
+  body("email")
+    .notEmpty()
+    .withMessage("Debes colocar un email")
+    .bail()
+    .isEmail()
+    .withMessage("Debes colocar un formato de email valido"),
+  body("password")
+    .notEmpty()
+    .withMessage("La contraseña debe de tener menos de 8 caracteres"),
 ];
 
-router.get('/login', loggedUserMiddleware, usersController.login);
-router.post('/login', usersController.processLogin);
+router.get("/login", loggedUserMiddleware, usersController.login);
+router.post("/login", usersController.processLogin);
 
-router.get('/register', loggedUserMiddleware, usersController.register);
-router.post('/register', upload.single('image'), validations, usersController.processRegister);
+router.get("/register", loggedUserMiddleware, usersController.register);
+router.post(
+  "/register",
+  upload.single("image"),
+  validations,
+  usersController.processRegister
+);
 
-router.get('/profile', profileAuthMiddleware, usersController.profile);
-router.get('/logout', usersController.logout);
+router.get("/profile", profileAuthMiddleware, usersController.profile);
+router.get("/logout", usersController.logout);
 
 module.exports = router;
