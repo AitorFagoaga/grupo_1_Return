0aa082cc484b6a47cd7af77261f4ab5613828a0c
AitorFagoaga <aitor.fagoaga2001@gmail.com>
2022-08-21 20:44:39 -0300

    focus en el nombre al agregar profucto

--- public/js/productsValidation.js
+++ public/js/productsValidation.js
@@ -1,42 +1,10 @@
-<<<<<<< HEAD
 window.onload = function () {
   let form = document.querySelector("#form");
-=======
-window.addEventListener("load", function (){
-    
-    let form = document.querySelector('.form');
-    form.name.focus();
-    form.addEventListener('submit', (e) => {
-        
-        let errors = [];
->>>>>>> df71209e41bb4e60d5cfe72a1947755c09ffef19
-
+  form.name.focus();
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-<<<<<<< HEAD
     let errors = [];
-=======
-        if (name.value == '' ) {
-            errors.push('El nombre no puede estar vacio');
-            if(name.value.length > 3){
-                errors.push('El nombre tiene que ser mayor a 3 caracteres');
-            }
-        } else {
-            form.price.focus();
-        };
-        if (price.value > 0) {
-            errors.push('El precio no puede ser 0');
-        } else {
-            form.description.focus();
-        };
-        if (description.value == '') {
-            errors.push('El producto debe tener una descripcion');
-        }
-        
-        
-        
->>>>>>> df71209e41bb4e60d5cfe72a1947755c09ffef19
 
     let name = document.querySelector("#name");
     let price = document.querySelector("#price");
